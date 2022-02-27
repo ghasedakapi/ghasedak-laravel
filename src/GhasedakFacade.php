485b3169d2d5e6972a6f7d3e0e61854b4bbfe27c
OmidMehri <thehopeofiran@gmail.com>
2022-02-27 17:00:13 +0330

    added message type constants

--- src/GhasedakFacade.php
+++ src/GhasedakFacade.php
@@ -11,24 +11,26 @@ use Illuminate\Support\Facades\Facade;
  *
  * @package Ghasedak\Laravel
  *
- * @method static array SendSimple($receptor, $message, $linenumber = null, $senddate = null, $checkid = null)
- * @method static array SendBulk($linenumber, $receptor, $message, $date, $checkid = null)
- * @method static array SendPair($linenumber, $receptor, $message, $date = null, $checkid = null)
- * @method static array Verify($receptor, $type, $template, ...$args)
- * @method static array Status($id, $type)
- * @method static array AddGroup($name, $parent = null)
- * @method static array AddNumber($groupid, $number, $firstname = null, $lastname = null, $email = null)
- * @method static array GroupList($parent = null)
- * @method static array GroupNumberList($groupid, $offset = null, $page = null)
- * @method static array GroupEdit($groupid, $name)
- * @method static array GroupRemove($groupid)
- * @method static array ReceiveSms($linenumber, $isread)
- * @method static array ReceivePaging($linenumber, $isread, $fromdate, $todate, $page, $offset)
- * @method static array CancelSms($messageid)
- * @method static array AccountInfo()
+ * @method static object SendSimple($receptor, $message, $linenumber = null, $senddate = null, $checkid = null)
+ * @method static object SendBulk($linenumber, $receptor, $message, $date, $checkid = null)
+ * @method static object SendPair($linenumber, $receptor, $message, $date = null, $checkid = null)
+ * @method static object Verify($receptor, $type, $template, ...$args)
+ * @method static object Status($id, $type)
+ * @method static object AddGroup($name, $parent = null)
+ * @method static object AddNumber($groupid, $number, $firstname = null, $lastname = null, $email = null)
+ * @method static object GroupList($parent = null)
+ * @method static object GroupNumberList($groupid, $offset = null, $page = null)
+ * @method static object GroupEdit($groupid, $name)
+ * @method static object GroupRemove($groupid)
+ * @method static object ReceiveSms($linenumber, $isread)
+ * @method static object ReceivePaging($linenumber, $isread, $fromdate, $todate, $page, $offset)
+ * @method static object CancelSms($messageid)
+ * @method static object AccountInfo()
  */
 class GhasedakFacade extends Facade
 {
+    const VERIFY_MESSAGE_TEXT = 1;
+    const VERIFY_MESSAGE_VOICE = 2;
     protected static function getFacadeAccessor()
     {
         return 'Laravel';
